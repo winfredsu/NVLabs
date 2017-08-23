6e857f645423dc695db872951197504f429191c3
unknown <>
2017-08-23 22:22:45 +0800

    app component

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	moduleId: module.id,
+	selector: 'nvlabs-app',
+	templateUrl: './app.component.html'
+})
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 // import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
 
+@NgModule({
+	imports: [
+		BrowserModule,
+		AppRoutingModule
+	],
+	declarations: [
+		AppComponent,
+	],
+	bootstrap: [AppComponent]
+})
+export class AppModule {}
